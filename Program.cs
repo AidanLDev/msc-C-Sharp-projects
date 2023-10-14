a6d7f5a65a1b014a2b43faa67905227493e2e964
AidanLDev <dev@aidanlowson.com>
2023-10-14 10:45:10 +0100

    Adding coin flip

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random coin = new Random();
+int coinToss = coin.Next(0, 2);
+Console.WriteLine($"Flipping coin... {(coinToss == 1 ? "Heads" : "Tails")}.");
+
+// https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/2-exercise-variable-scope
---
+++ heads-and-tails/HeadsAndTails.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace headsAndTails
+{
+    class coinToss
+    {
+        static void Main()
+        {
+            Random coin = new Random();
+            int coinToss = coin.Next(1, 3);
+            Console.WriteLine($"Flipping coin... {(coinToss == 1 ? "Heads" : "Tails")}.");
+        }
+
+    }
+
+}
+
+
