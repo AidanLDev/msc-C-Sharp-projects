93c07b39ad22b010a92b86c393e5a6c43cc54818
Aidan <aidan.lowson@davies-group.com>
2023-12-15 11:08:11 +0000

    Finishing game

--- Program.cs
+++ Program.cs
@@ -4,8 +4,31 @@ class Program
 {
     static void Main()
     {
-        Character char1 = new Character();
-        char1.Name = "Aidan";
-        Console.WriteLine(char1.Name);
+
+    }
+
+    static void CallFightingGame()
+    {
+        Character mage = new Character("Merlin", 20, 5, 100);
+        Character dragon = new Character("Kalemite", 100, 500, 2000);
+        Character warrior = new Character("Aragon", 15, 60, 650);
+
+        while (mage.Health > 0 && warrior.Health > 0)
+        {
+            mage.Attack(warrior);
+            if (warrior.Health > 0)
+            {
+                warrior.Attack(mage);
+            }
+        }
+
+        while (dragon.Health > 0 && warrior.Health > 0)
+        {
+            warrior.Attack(dragon);
+            if (dragon.Health > 0)
+            {
+                dragon.Attack(warrior);
+            }
+        }
     }
 }
